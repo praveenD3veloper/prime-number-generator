8efb10dff9f8c9f7830701ef9b88cff0f5690ba7
Praveenkumar Nagaraj <praveenero38@gmail.com>
2025-07-06 15:42:19 +0100

    Added springboot 3.4.x skeleton for the project with Java21

---
+++ src/main/java/com/prav/prime/PrimeNumberGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.prav.prime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimeNumberGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimeNumberGeneratorApplication.class, args);
+	}
+
+}
