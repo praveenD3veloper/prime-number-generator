21db637d86c4c9161999829eabf90c5c767b33c6
Praveenkumar Nagaraj <praveenero38@gmail.com>
2025-07-08 11:17:57 +0100

    Added 2 algorithms to core package

--- src/main/java/com/prav/prime/core/StrategyConfig.java
+++ src/main/java/com/prav/prime/core/StrategyConfig.java
@@ -1,6 +1,8 @@
 package com.prav.prime.core;
 
-import com.prav.prime.core.algorithm.BruteForcePrimeGenerator;
+import com.prav.prime.core.algorithm.BruteForce;
+import com.prav.prime.core.algorithm.SieveOfEratosthenes;
+import com.prav.prime.core.algorithm.SieveOfEratosthenesParallel;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,6 +15,25 @@ public class StrategyConfig {
      */
     @Bean
     public PrimeGenerator bruteForce() {
-        return new BruteForcePrimeGenerator();
+        return new BruteForce();
     }
+
+    /**
+     *
+     * @return sieve of eratosthenes bean
+     */
+    @Bean
+    public PrimeGenerator sieveOfEratosthenes() {
+        return new SieveOfEratosthenes();
+    }
+
+    /**
+     *
+     * @return sieve of eratosthenes parallel execution bean
+     */
+    @Bean
+    public PrimeGenerator sieveOfEratosthenesInParallel() {
+        return new SieveOfEratosthenesParallel();
+    }
+
 }
