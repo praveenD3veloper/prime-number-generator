c1c1a66dd2e485d3e5953d5bde8597d67a507e9e
Praveenkumar Nagaraj <praveenero38@gmail.com>
2025-07-10 17:14:40 +0100

    checkstyle issues fixed #3

--- src/main/java/com/prav/prime/cache/CacheConfig.java
+++ src/main/java/com/prav/prime/cache/CacheConfig.java
@@ -1,13 +1,11 @@
 package com.prav.prime.cache;
 
-import com.github.benmanes.caffeine.cache.Caffeine;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.caffeine.CaffeineCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.util.concurrent.TimeUnit;
 
 /**
  * CacheConfig configures a {@link CacheManager} backed by Caffeine for use in the Spring context.
