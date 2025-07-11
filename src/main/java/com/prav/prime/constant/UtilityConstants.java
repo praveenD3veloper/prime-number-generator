37040cbdaf350caf8e0489c4f6dc30498fc8d902
Praveenkumar Nagaraj <praveenero38@gmail.com>
2025-07-11 13:42:03 +0100

    upadted max request limit

--- src/main/java/com/prav/prime/constant/UtilityConstants.java
+++ src/main/java/com/prav/prime/constant/UtilityConstants.java
@@ -5,8 +5,8 @@ public class UtilityConstants {
     /**
      * The maximum number of allowed requests per minute per IP address.
      */
-    public static final int MAX_REQUESTS_PER_MINUTE = 5;
-    public static final long MIN_INTERVAL_BETWEEN_REQUESTS_MS = 1000; // 0.5 second
+    public static final int MAX_REQUESTS_PER_MINUTE = 10;
+    public static final long MIN_INTERVAL_BETWEEN_REQUESTS_MS = 1000; // 1 second
     public static final int MAX_RANGE = 999999;
     public static final String BASE_URL = "/primes";
     public static final int MAX_SIZE = 1000;
